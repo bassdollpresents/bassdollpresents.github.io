69e286490f7fc445b1c3d7d62a6abae3f6dcd85e
stzuko <sto.lat.syd@gmail.com>
2019-10-09 23:42:44 -0500

    font idea, css edits

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <Header>
-          <p>BassDoll Music</p>
-          <p>Links go here</p>
+          b a s s d o l l
         </Header>
         <Body>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -36,7 +35,11 @@ class App extends Component {
 export default App;
 
 const HeaderWrapper = styled.div`
+  padding: 10px;
   text-align: center;
+  font-family: 'UnifrakturMaguntia', cursive;
+  font-size: 30px;
+  color: white;
   background-color: black;
 `;
 
